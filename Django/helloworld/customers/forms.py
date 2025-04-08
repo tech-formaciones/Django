8c132b97fc91d65780eb11973ad35c329917a00e
Borja Cabeza <borjacabezarozas@gmail.com>
2025-04-08 20:02:07 +0200

    Sesion 5d

--- Django/helloworld/customers/forms.py
+++ Django/helloworld/customers/forms.py
@@ -5,3 +5,13 @@ class CustomersForm(forms.ModelForm):
     class Meta:
         model = Customer
         fields = '__all__'
+        widgets = {
+            'CustomerID': forms.TextInput(attrs={'class': 'form-control'}),
+            'CompanyName': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            
+            for field in self.fields.values():
+                field.widget.attrs.update({'class': 'form-control'})
--- Django/helloworld/customers/views.py
+++ Django/helloworld/customers/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse, reverse_lazy
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Customer
 from .forms import CustomersForm
@@ -14,5 +16,21 @@ def customer_detail(request, customer_id):
     form = CustomersForm(instance=customer)
 
     return render(request, 'customers_detail.html', {'customer' : customer, 'form': form})
+
+def customer_create(request):
+    if(request.method == 'POST'):
+        Customer.objects.create(
+            CustomerID = request.POST['CustomerID'],
+            CompanyName = request.POST['CompanyName'],
+            ContactName = request.POST['ContactName'],
+            ContactTitle = request.POST['ContactTitle'],
+            Address = request.POST['Address'],
+        )
+
+        return HttpResponseRedirect('/customer')                  # Opción 1: Especificamos la URL completa
+        return HttpResponseRedirect(reverse('customers_list'))    # Opción 2: Especificamos el name de la ruta
+    else:
+        form = CustomersForm()
+        return render(request, 'customers_create.html', { 'form' : form})
 
 def temp(request): pass
